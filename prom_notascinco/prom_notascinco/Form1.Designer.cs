bd97d4f2bdcb02e900dc286f2da29b8ea212e262
michael escobar galeano <maicolgemelo@gmail.com>
2019-09-06 19:40:54 -0500

    programa que calcula el resultado de un estudiante

---
+++ prom_notascinco/prom_notascinco/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace prom_notascinco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.txtcalif1 = new System.Windows.Forms.TextBox();
+            this.txtcalif2 = new System.Windows.Forms.TextBox();
+            this.txtcalif3 = new System.Windows.Forms.TextBox();
+            this.txtcalif4 = new System.Windows.Forms.TextBox();
+            this.txtcalif5 = new System.Windows.Forms.TextBox();
+            this.lblcalif1 = new System.Windows.Forms.Label();
+            this.lblcalif2 = new System.Windows.Forms.Label();
+            this.lblcalif3 = new System.Windows.Forms.Label();
+            this.lblcalif4 = new System.Windows.Forms.Label();
+            this.lblcalif5 = new System.Windows.Forms.Label();
+            this.lblprom = new System.Windows.Forms.Label();
+            this.lblrespuesta = new System.Windows.Forms.Label();
+            this.lblpaso = new System.Windows.Forms.Label();
+            this.lblcalifi5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.Location = new System.Drawing.Point(534, 174);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(97, 61);
+            this.btncalcular.TabIndex = 0;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.Location = new System.Drawing.Point(250, 20);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(240, 24);
+            this.lbltitulo.TabIndex = 1;
+            this.lbltitulo.Text = "Resultado del estudiante";
+            // 
+            // txtcalif1
+            // 
+            this.txtcalif1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcalif1.Location = new System.Drawing.Point(350, 88);
+            this.txtcalif1.Name = "txtcalif1";
+            this.txtcalif1.Size = new System.Drawing.Size(100, 24);
+            this.txtcalif1.TabIndex = 2;
+            // 
+            // txtcalif2
+            // 
+            this.txtcalif2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcalif2.Location = new System.Drawing.Point(350, 132);
+            this.txtcalif2.Name = "txtcalif2";
+            this.txtcalif2.Size = new System.Drawing.Size(100, 24);
+            this.txtcalif2.TabIndex = 3;
+            // 
+            // txtcalif3
+            // 
+            this.txtcalif3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcalif3.Location = new System.Drawing.Point(350, 187);
+            this.txtcalif3.Name = "txtcalif3";
+            this.txtcalif3.Size = new System.Drawing.Size(100, 24);
+            this.txtcalif3.TabIndex = 4;
+            // 
+            // txtcalif4
+            // 
+            this.txtcalif4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcalif4.Location = new System.Drawing.Point(350, 233);
+            this.txtcalif4.Name = "txtcalif4";
+            this.txtcalif4.Size = new System.Drawing.Size(100, 24);
+            this.txtcalif4.TabIndex = 5;
+            // 
+            // txtcalif5
+            // 
+            this.txtcalif5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtcalif5.Location = new System.Drawing.Point(350, 297);
+            this.txtcalif5.Name = "txtcalif5";
+            this.txtcalif5.Size = new System.Drawing.Size(100, 24);
+            this.txtcalif5.TabIndex = 6;
+            // 
+            // lblcalif1
+            // 
+            this.lblcalif1.AutoSize = true;
+            this.lblcalif1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcalif1.Location = new System.Drawing.Point(203, 88);
+            this.lblcalif1.Name = "lblcalif1";
+            this.lblcalif1.Size = new System.Drawing.Size(126, 18);
+            this.lblcalif1.TabIndex = 7;
+            this.lblcalif1.Text = "calificacion  #1:";
+            // 
+            // lblcalif2
+            // 
+            this.lblcalif2.AutoSize = true;
+            this.lblcalif2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcalif2.Location = new System.Drawing.Point(198, 132);
+            this.lblcalif2.Name = "lblcalif2";
+            this.lblcalif2.Size = new System.Drawing.Size(131, 18);
+            this.lblcalif2.TabIndex = 8;
+            this.lblcalif2.Text = "calificacion  #2 :";
+            // 
+            // lblcalif3
+            // 
+            this.lblcalif3.AutoSize = true;
+            this.lblcalif3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcalif3.Location = new System.Drawing.Point(198, 186);
+            this.lblcalif3.Name = "lblcalif3";
+            this.lblcalif3.Size = new System.Drawing.Size(131, 18);
+            this.lblcalif3.TabIndex = 9;
+            this.lblcalif3.Text = "calificacion  #3 :";
+            // 
+            // lblcalif4
+            // 
+            this.lblcalif4.AutoSize = true;
+            this.lblcalif4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcalif4.Location = new System.Drawing.Point(198, 232);
+            this.lblcalif4.Name = "lblcalif4";
+            this.lblcalif4.Size = new System.Drawing.Size(131, 18);
+            this.lblcalif4.TabIndex = 10;
+            this.lblcalif4.Text = "calificacion  #4 :";
+            // 
+            // lblcalif5
+            // 
+            this.lblcalif5.AutoSize = true;
+            this.lblcalif5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcalif5.Location = new System.Drawing.Point(251, 297);
+            this.lblcalif5.Name = "lblcalif5";
+            this.lblcalif5.Size = new System.Drawing.Size(0, 18);
+            this.lblcalif5.TabIndex = 11;
+            // 
+            // lblprom
+            // 
+            this.lblprom.AutoSize = true;
+            this.lblprom.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblprom.Location = new System.Drawing.Point(241, 372);
+            this.lblprom.Name = "lblprom";
+            this.lblprom.Size = new System.Drawing.Size(89, 18);
+            this.lblprom.TabIndex = 12;
+            this.lblprom.Text = "promedio :";
+            // 
+            // lblrespuesta
+            // 
+            this.lblrespuesta.AutoSize = true;
+            this.lblrespuesta.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblrespuesta.Location = new System.Drawing.Point(357, 372);
+            this.lblrespuesta.Name = "lblrespuesta";
+            this.lblrespuesta.Size = new System.Drawing.Size(88, 18);
+            this.lblrespuesta.TabIndex = 13;
+            this.lblrespuesta.Text = "Respuesta";
+            this.lblrespuesta.Visible = false;
+            // 
+            // lblpaso
+            // 
+            this.lblpaso.AutoSize = true;
+            this.lblpaso.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpaso.Location = new System.Drawing.Point(472, 372);
+            this.lblpaso.Name = "lblpaso";
+            this.lblpaso.Size = new System.Drawing.Size(45, 18);
+            this.lblpaso.TabIndex = 14;
+            this.lblpaso.Text = "paso";
+            this.lblpaso.Visible = false;
+            // 
+            // lblcalifi5
+            // 
+            this.lblcalifi5.AutoSize = true;
+            this.lblcalifi5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcalifi5.Location = new System.Drawing.Point(203, 296);
+            this.lblcalifi5.Name = "lblcalifi5";
+            this.lblcalifi5.Size = new System.Drawing.Size(126, 18);
+            this.lblcalifi5.TabIndex = 15;
+            this.lblcalifi5.Text = "calificacion #5 :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblcalifi5);
+            this.Controls.Add(this.lblpaso);
+            this.Controls.Add(this.lblrespuesta);
+            this.Controls.Add(this.lblprom);
+            this.Controls.Add(this.lblcalif5);
+            this.Controls.Add(this.lblcalif4);
+            this.Controls.Add(this.lblcalif3);
+            this.Controls.Add(this.lblcalif2);
+            this.Controls.Add(this.lblcalif1);
+            this.Controls.Add(this.txtcalif5);
+            this.Controls.Add(this.txtcalif4);
+            this.Controls.Add(this.txtcalif3);
+            this.Controls.Add(this.txtcalif2);
+            this.Controls.Add(this.txtcalif1);
+            this.Controls.Add(this.lbltitulo);
+            this.Controls.Add(this.btncalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Label lbltitulo;
+        private System.Windows.Forms.TextBox txtcalif1;
+        private System.Windows.Forms.TextBox txtcalif2;
+        private System.Windows.Forms.TextBox txtcalif3;
+        private System.Windows.Forms.TextBox txtcalif4;
+        private System.Windows.Forms.TextBox txtcalif5;
+        private System.Windows.Forms.Label lblcalif1;
+        private System.Windows.Forms.Label lblcalif2;
+        private System.Windows.Forms.Label lblcalif3;
+        private System.Windows.Forms.Label lblcalif4;
+        private System.Windows.Forms.Label lblcalif5;
+        private System.Windows.Forms.Label lblprom;
+        private System.Windows.Forms.Label lblrespuesta;
+        private System.Windows.Forms.Label lblpaso;
+        private System.Windows.Forms.Label lblcalifi5;
+    }
+}
+
---
+++ prom_notascinco/prom_notascinco/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prom_notascinco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nota1 = txtcalif1.Text;
+            string nota2 = txtcalif2.Text;
+            string nota3 = txtcalif3.Text;
+            string nota4 = txtcalif4.Text;
+            string nota5 = txtcalif5.Text;
+            string promedio = lblrespuesta.Text; 
+
+            float calif1 = float.Parse(nota1);
+            float calif2 = float.Parse(nota2);
+            float calif3 = float.Parse(nota3);
+            float calif4 = float.Parse(nota4);
+            float calif5 = float.Parse(nota5);
+            float prom = (calif1 + calif2 + calif3 + calif4 + calif5) / 5;
+
+            if (prom >= 3.5)
+            {
+
+                lblrespuesta.Text = prom.ToString();
+                lblpaso.Text = " has superado la materia ";
+                lblrespuesta.Visible = true;
+                lblpaso.Visible = true;
+                lblpaso.ForeColor = Color.Blue;
+
+            }
+            else
+            {
+
+                lblrespuesta.Text = prom.ToString();
+                lblpaso.Text = " No superaste la competencia de la materia";
+                lblrespuesta.Visible = true;
+                lblpaso.Visible = true;
+                lblpaso.ForeColor = Color.Red;
+            }
+        }
+    }
+}
